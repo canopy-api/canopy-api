701b0d89af31e7dd7599a0aceb5e54831ab7e030
Ryan Anderson <andersra88@gmail.com>
2022-12-19 22:54:38 -0500

    fix colors

--- docs/src/components/Callout.jsx
+++ docs/src/components/Callout.jsx
@@ -7,13 +7,13 @@ const styles = {
     container:
       'bg-green-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10',
     title: 'text-green-900 dark:text-green-400',
-    body: 'text-green-800 [--tw-prose-background:theme(colors.sky.50)] prose-a:text-green-900 prose-code:text-green-900 dark:text-slate-300 dark:prose-code:text-slate-300',
+    body: 'text-green-800 [--tw-prose-background:theme(colors.green.50)] prose-a:text-green-900 prose-code:text-green-900 dark:text-slate-300 dark:prose-code:text-slate-300',
   },
   warning: {
     container:
       'bg-amber-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10',
     title: 'text-amber-900 dark:text-amber-500',
-    body: 'text-amber-800 [--tw-prose-underline:theme(colors.amber.400)] [--tw-prose-background:theme(colors.amber.50)] prose-a:text-amber-900 prose-code:text-amber-900 dark:text-slate-300 dark:[--tw-prose-underline:theme(colors.sky.700)] dark:prose-code:text-slate-300',
+    body: 'text-amber-800 [--tw-prose-underline:theme(colors.amber.400)] [--tw-prose-background:theme(colors.amber.50)] prose-a:text-amber-900 prose-code:text-amber-900 dark:text-slate-300 dark:[--tw-prose-underline:theme(colors.green.700)] dark:prose-code:text-slate-300',
   },
 }
 
